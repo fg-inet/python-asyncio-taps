0bf34100ea69b6701bc1ee183f99820723627c97
Theresa Enghardt <theresa@inet.tu-berlin.de>
2019-07-20 15:38:30 -0400

    echoServer: Allow listening for TCP and UDP at the same time

--- examples/echo_example/echoServer.py
+++ examples/echo_example/echoServer.py
@@ -8,10 +8,14 @@ color = "blue"
 
 
 class TestServer():
-    def __init__(self):
+    """A simple echo server. Listens using TCP by default, but can also use UDP.
+
+    """
+    def __init__(self, reliable = True):
         self.preconnection = None
         self.loop = asyncio.get_event_loop()
         self.connection = None
+        self.reliable = reliable
 
     async def handle_connection_received(self, connection):
         taps.print_time("Received new Connection.", color)
@@ -19,7 +23,7 @@ class TestServer():
         self.connection.on_received_partial(self.handle_received_partial)
         self.connection.on_received(self.handle_received)
         self.connection.on_sent(self.handle_sent)
-        await self.connection.receive(min_incomplete_length=7, max_length=-1)
+        await self.connection.receive(min_incomplete_length=1, max_length=-1)
         # await self.connection.receive(min_incomplete_length=4, max_length=3)
         # self.connection.on_sent(handle_sent)
 
@@ -69,7 +73,8 @@ class TestServer():
             taps.print_time("Created SecurityParameters.", color)
 
         tp = taps.TransportProperties()
-        # tp.prohibit("reliability")
+        if self.reliable == False:
+            tp.prohibit("reliability")
         tp.ignore("congestion-control")
         tp.ignore("preserve-order")
 
@@ -96,9 +101,16 @@ if __name__ == "__main__":
     ap.add_argument('--trust-ca', type=str, default=None)
     ap.add_argument('--secure', '-s', nargs='?', const=True, type=bool,
                     default=False)
+    ap.add_argument('--reliable', type=str, default="yes")
     args = ap.parse_args()
     print(args)
+
+    loop = asyncio.get_event_loop()
     # Start testserver
-    server = TestServer()
-    server.loop.create_task(server.main(args))
-    server.loop.run_forever()
+    if args.reliable in [ "yes", "true", "both" ]:
+        server_tcp = TestServer(reliable = True)
+        loop.create_task(server_tcp.main(args))
+    if args.reliable in [ "no", "false", "both" ]:
+        server_udp = TestServer(reliable = False)
+        loop.create_task(server_udp.main(args))
+    loop.run_forever()
