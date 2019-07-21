8a9031d37c99bb7628929baada62545690618f28
Theresa Enghardt <theresa@inet.tu-berlin.de>
2019-07-21 10:00:17 -0400

    Add TLS tests, make algorithm and identity optional in YANG model

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,24 +5,26 @@ import sys
 sys.path.append(sys.path[0] + "/..")
 import PyTAPS as taps  # noqa: E402
 
-from threading import Thread
-
 import time
 
 TEST_TIMEOUT = 5
 
 class TestClient():
 
+	async def handle_closed(self):
+		print("Closed")
+		asyncio.get_event_loop().stop()
+
 	async def handle_received(self, data, context, connection):
 		print("Receive message: " + str(data))
 		self.received_data = data
-		asyncio.get_event_loop().stop()
+		connection.close()
 
 	async def handle_received_partial(self, data, context, end_of_message,
 									  connection):
 		print("Receive partial message: " + str(data))
 		self.received_data = data
-		asyncio.get_event_loop().stop()
+		connection.close()
 
 	async def sent_and_receive(self, connection):
 		print("Sent and receive")
@@ -39,58 +41,77 @@ class TestClient():
 			connection.on_sent(self.sent_and_receive)
 		self.connection.on_received(self.handle_received)
 		self.connection.on_received_partial(self.handle_received_partial)
+		self.connection.on_closed(self.handle_closed)
 		msgref = await self.connection.send_message(self.data_to_send)
 
-	async def main(self, data_to_send="Hello\n", stop_at_sent=True, yangfile=None):
+	async def main(self, data_to_send="Hello\n", stop_at_sent=False, remote_port=6666, reliable=False, yangfile=None, trust_ca=None, local_identity=None):
 		self.data_to_send = data_to_send
 		self.stop_at_sent = stop_at_sent
 		self.yangfile = yangfile
 
 		ep = taps.RemoteEndpoint()
-		ep.with_address("localhost")
-		ep.with_port(6666)
+		ep.with_hostname("localhost")
+		ep.with_port(remote_port)
 		tp = taps.TransportProperties()
 
-		tp.prohibit("reliability")
+		if not reliable:
+			tp.prohibit("reliability")
 		tp.ignore("congestion-control")
 		tp.ignore("preserve-order")
 
+		if trust_ca or local_identity:
+			sp = taps.SecurityParameters()
+			if trust_ca:
+				sp.addTrustCA(trust_ca)
+			if local_identity:
+				sp.addIdentity(local_identity)
+		else:
+			sp = None
+
 		self.preconnection = taps.Preconnection(remote_endpoint=ep,
 												transport_properties=tp,
+												security_parameters=sp,
 												event_loop=asyncio.get_event_loop(),
 												yangfile=yangfile)
 		self.preconnection.on_ready(self.handle_ready)
 		self.connection = await self.preconnection.initiate()
 
+# Send something over UDP, then stop event loop
 @pytest.mark.timeout(TEST_TIMEOUT)
 def test_sending():
 	loop = asyncio.new_event_loop()
 	try:
 		client = TestClient()
 		asyncio.set_event_loop(loop)
-		task = loop.create_task(client.main())
+		task = loop.create_task(client.main(stop_at_sent=True))
 		loop.run_forever()
 
 		assert True
 	finally:
 		loop.close()
 
+# Send something over UDP, receive something, check if it's the same
+# This requires an echo server to listen on UDP port 6666
+# python3.7 examples/echo_example/echoServer.py --local-address=::1 --local-port=6666  --reliable both
 @pytest.mark.timeout(TEST_TIMEOUT)
-def test_echo():
+def test_echo_udp():
 	teststring = "Hello\n"
 	loop = asyncio.new_event_loop()
 	try:
 		client = TestClient()
 		asyncio.set_event_loop(loop)
-		task = loop.create_task(client.main(data_to_send=teststring, stop_at_sent=False))
+		task = loop.create_task(client.main(data_to_send=teststring))
 		loop.run_forever()
 
 		assert client.received_data == teststring
 	finally:
 		loop.close()
 
+# Send something over UDP, receive something, check if it's the same
+# This requires an echo server to listen on UDP port 6666
+# e.g.: python3.7 examples/echo_example/echoServer.py --local-address=::1 --local-port=6666  --reliable both
 @pytest.mark.timeout(TEST_TIMEOUT)
-def test_echo_yang():
+def test_echo_udp_yang():
 	teststring = "Hello\n"
 	yangfile_client = "udp-client.json"
 
@@ -98,7 +119,7 @@ def test_echo_yang():
 	try:
 		client = TestClient()
 		asyncio.set_event_loop(client_loop)
-		task = client_loop.create_task(client.main(data_to_send=teststring, stop_at_sent=False, yangfile=yangfile_client))
+		task = client_loop.create_task(client.main(data_to_send=teststring, yangfile=yangfile_client))
 
 		client_loop.run_forever()
 		print("Started client")
@@ -107,6 +128,9 @@ def test_echo_yang():
 	finally:
 		client_loop.close()
 
+# Send something over TCP, receive something, check if it's the same
+# This requires an echo server to listen on TCP port 6666
+# e.g.: python3.7 examples/echo_example/echoServer.py --local-address=::1 --local-port=6666  --reliable both
 @pytest.mark.timeout(TEST_TIMEOUT)
 def test_echo_yang_tcp():
 	teststring = "Hello\n"
@@ -115,7 +139,42 @@ def test_echo_yang_tcp():
 	try:
 		client = TestClient()
 		asyncio.set_event_loop(loop)
-		task = loop.create_task(client.main(data_to_send=teststring, stop_at_sent=False, yangfile=yangfile))
+		task = loop.create_task(client.main(data_to_send=teststring, yangfile=yangfile))
+		loop.run_forever()
+
+		assert client.received_data == teststring
+	finally:
+		loop.close()
+
+# Send something over TLS, receive something, check if it's the same
+# This requires an echo server to listen on TLS port 6667
+# e.g.: python3.7 examples/echo_example/echoServer.py --local-address=::1 --local-port=6667 --local-identity tests/keys/localhost.pem
+@pytest.mark.timeout(TEST_TIMEOUT)
+def test_echo_tls():
+	teststring = "Hello\n"
+	loop = asyncio.new_event_loop()
+	try:
+		client = TestClient()
+		asyncio.set_event_loop(loop)
+		task = loop.create_task(client.main(data_to_send=teststring, remote_port=6667, reliable=True, trust_ca="keys/MyRootCA.pem"))
+		loop.run_forever()
+
+		assert client.received_data == teststring
+	finally:
+		loop.close()
+
+# Send something over TLS, receive something, check if it's the same
+# This requires an echo server to listen on TLS port 6667
+# e.g.: python3.7 examples/echo_example/echoServer.py --local-address=::1 --local-port=6667 --local-identity tests/keys/localhost.pem
+@pytest.mark.timeout(TEST_TIMEOUT)
+def test_echo_tls_yang():
+	teststring = "Hello\n"
+	yangfile = "tls-client.json"
+	loop = asyncio.new_event_loop()
+	try:
+		client = TestClient()
+		asyncio.set_event_loop(loop)
+		task = loop.create_task(client.main(data_to_send=teststring, yangfile=yangfile))
 		loop.run_forever()
 
 		assert client.received_data == teststring
